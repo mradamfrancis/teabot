2ae2d06971e2a4af423bffb21667c37d81fde4ca
mradamfrancis <ajfrancis@gmail.com>
2017-01-30 22:37:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ slapp.message('tea',['ambient', 'mention'], (msg) => {
     startTeaState = false
   } else {
     msg.say('<!channel> time for tea!!! - who wants in?')
-    msg.say(msg.body.event.user_name)
+    msg.say(msg.body.event.user.name)
     startTeaState = true
     //teaUsers
   }
