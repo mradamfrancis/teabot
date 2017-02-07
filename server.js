2dd4e191e2e698085b8f197e06c8030904f29d2a
mradamfrancis <ajfrancis@gmail.com>
2017-02-07 14:05:44 +0000

    change limit to two mins

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ slapp.message('^(tea|t|:tea:)$',['ambient', 'mention'], (msg) => {
     startTeaState = true
     
     setTimeout(() => {
-    msg.say('30 seconds left - any one else?')
+    msg.say('1 minute left - any one else?')
       setTimeout(() => {
         startTeaState = false
         if (teaUsers.length != 0) {
@@ -96,8 +96,8 @@ slapp.message('^(tea|t|:tea:)$',['ambient', 'mention'], (msg) => {
             msg.say(teaMaker + ' you\'re making tea for yourself')
           }
         }
-  }, 30000)
-  }, 30000)
+  }, 60000)
+  }, 60000)
     //teaUsers
   }
   })
