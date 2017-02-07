537cae78026eef325943742d64155fa805a0e0ce
JordanLapointe <jordan@bloomandwild.com>
2017-02-07 15:14:12 +0000

    This probably won't work, but it is worth a go Adam lol

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express')
 const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
 const Context = require('slapp-context-beepboop')
+const redis = require("redis");
+const client = redis.createClient({
+  url: 'redis://jordanlapointe:42d168c1768236f5665321416a4a2ee3@50.30.35.9:3349/'
+});
 
 // use `PORT` env var on Beep Boop - default to 3000 locally
 var port = process.env.PORT || 3000
@@ -46,8 +50,7 @@ slapp.message('^(me|yes|y|ye|yeah)$',['ambient', 'mention'], (msg) => {
 })
 
 slapp.message('^(set).*',['ambient', 'mention'], (msg) => {
-    //msg.body.event.user
-
+    client.set(msg.body.event.user, msg.body.event.text.substring(0, msg.body.event.text.indexOf("set")));
 })
 
 slapp.message('^(tea|t|:tea:)$',['ambient', 'mention'], (msg) => {
@@ -100,6 +103,13 @@ slapp.message('^(tea|t|:tea:)$',['ambient', 'mention'], (msg) => {
           } else {
             msg.say(teaMaker + ' you\'re making tea for yourself')
           }
+          uniqueNames.forEach(function(name) {
+            client.get(name, function (err, reply) {
+                if (reply) {
+                  msg.say(name + ' - ' + reply.toString());
+                }
+            });
+          });
         }
   }, 60000)
   }, 60000)
