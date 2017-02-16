2ca7939afc44a1cdee245157ddf5984f1a22b665
mradamfrancis <ajfrancis@gmail.com>
2017-02-16 09:19:17 +0000

    Update server.js

--- server.js
+++ server.js
@@ -201,7 +201,7 @@ slapp.message('^(tea|t|:tea:)$',['ambient', 'mention'], (msg) => {
                 console.log(`nothing in db for: `, name);
                 if (reply) {
                   console.log(`Pref in db for: `, name, reply);
-                  var user = '<' + name + '>'
+                  var user = '<@' + name + '>'
                   msg.say(user + ' - ' + reply.toString());
                 }
             });
